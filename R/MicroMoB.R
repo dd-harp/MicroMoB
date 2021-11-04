787aff7868f811b944ad8361301d6746f51237e5
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-11-04 15:43:27 -0700

    package mockup

---
+++ R/MicroMoB.R
@@ -0,0 +1,10 @@
+#' MicroMoB: Discrete time simulation of mosquito-borne pathogen transmission
+#'
+#' This package does things.
+#'
+#' @section MicroMoB functions:
+#' The MicroMoB functions ...
+#'
+#' @docType package
+#' @name MicroMoB
+NULL
