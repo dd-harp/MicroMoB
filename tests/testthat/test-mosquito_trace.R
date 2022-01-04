21991cc5309d07c29862a172642502693aff1f4d
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-01-04 11:24:41 -0800

    tests for BH aquatic model

--- tests/testthat/test-mosquito_trace.R
+++ tests/testthat/test-mosquito_trace.R
@@ -9,10 +9,20 @@ test_that("null mosquito trace works", {
   setup_mosquito_trace(model = mod, oviposit = 5)
   expect_equal(compute_oviposit(mod), 5)
 
+  expect_error(step_mosquitoes(mod))
+  expect_error(compute_f(mod))
+  expect_error(compute_q(mod))
+  expect_error(compute_Z(mod))
+
   p <- 3
   tmax <- 10
   mod <- make_MicroMoB(tmax = tmax, p = p)
   expect_error(setup_mosquito_trace(model = mod, oviposit = 5))
   setup_mosquito_trace(model = mod, oviposit = c(1,5,10))
   expect_equal(compute_oviposit(mod), c(1,5,10))
+
+  expect_error(step_mosquitoes(mod))
+  expect_error(compute_f(mod))
+  expect_error(compute_q(mod))
+  expect_error(compute_Z(mod))
 })
