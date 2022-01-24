7b01197826d2055eac49cf839f84582505082059
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-01-24 15:42:04 -0800

    export internal utils

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -1,6 +1,9 @@
-# utilities that are not documented or exported
+# utilities that are not documented
 
-#' @noRd
+#' @title Does a numeric object consist of only zeros and ones?
+#' @param x a [numeric] object
+#' @return a logical value
+#' @export
 is_binary <- function(x) {
   uniq <- unique(as.vector(x))
   if (all(length(uniq) == 2L)) {
@@ -11,13 +14,22 @@ is_binary <- function(x) {
 }
 
 
-#' @noRd
+#' @title Check if two numeric values are approximately equal
+#' @param a a [numeric] object
+#' @param b a [numeric] object
+#' @param tol the numeric tolerance
+#' @return a logical value
+#' @export
 approx_equal <- function(a, b, tol = sqrt(.Machine$double.eps)) {
   abs(a - b) < tol
 }
 
 
-#' @noRd
+#' @title Division of integers
+#' @param a the dividend
+#' @param b the divisor
+#' @return a list with two elements, `quo` (quotient) and `rem` (remainder)
+#' @export
 divmod <- function(a,b){
   a <- as.integer(a)
   b <- as.integer(b)
@@ -28,7 +40,11 @@ divmod <- function(a,b){
 }
 
 
-#' @noRd
+#' @title Distribute items into bins as evenly as possible
+#' @param n number of bins
+#' @param p number of itens
+#' @return a numeric vector of bin sizes
+#' @export
 distribute <- function(n,p){
   n <- as.integer(n)
   p <- as.integer(p)
@@ -38,65 +54,4 @@ distribute <- function(n,p){
     distn[i+1] <- div[["quo"]] + (i < div[["rem"]])
   }
   return(distn)
-}
-
-
-#' @title Input parameters that may vary by time and patch
-#' @param param if given a matrix, it must have nrows equal to `p` and ncols equal to
-#' either `tmax` or `365`; if given a vector it must be of length `p`, `tmax`, or `365`.
-#' @param p number of patches
-#' @param tmax number of time steps
-time_patch_varying_parameter <- function(param, p, tmax) {
-  if (inherits(param, "matrix")) {
-    stopifnot(nrow(param) == p)
-    if (ncol(param) == 365L) {
-      ix <- (1:tmax) %% 365L
-      ix[which(ix == 0L)] <- 365L
-      out <- param[, ix, drop = FALSE]
-    } else if (ncol(param) == tmax) {
-      out <- param
-    } else {
-      stop("incorrect dimensions of parameter")
-    }
-  } else {
-    # vector input
-    if (length(param) == p) {
-      if (p > 1) {
-        out <- replicate(n = tmax, expr = param)
-      } else {
-        out <- matrix(data = param, nrow = 1, ncol = tmax)
-      }
-    } else if (length(param) == tmax) {
-      out <- do.call(rbind, replicate(n = p, expr = param, simplify = FALSE))
-    } else if (length(param) == 365L) {
-      ix <- (1:tmax) %% 365L
-      ix[which(ix == 0L)] <- 365L
-      out <- do.call(rbind, replicate(n = p, expr = param[ix], simplify = FALSE))
-    } else {
-      stop("incorrect length of parameter")
-    }
-  }
-  stopifnot(nrow(out) == p)
-  stopifnot(ncol(out) == tmax)
-  return(out)
-}
-
-
-#' @title Input parameters that may vary by time
-#' @param param a vector of length `1`, `tmax`, or `365`.
-#' @param tmax number of time steps
-time_varying_parameter <- function(param, tmax) {
-  if (length(param) == 1L) {
-    out <- rep(param, tmax)
-  } else if(length(param) == 365L) {
-    ix <- (1:tmax) %% 365L
-    ix[which(ix == 0L)] <- 365L
-    out <- param[ix]
-  } else if(length(param) == tmax) {
-    out <- param
-  } else {
-    stop("incorrect length of parameter")
-  }
-  stopifnot(length(out) == tmax)
-  return(out)
 }
